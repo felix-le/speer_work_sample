5f3bb84a718bb25ca8167fcd1ab5f2819f325080
Viet Anh Le <mc.vietanh@gmail.com>
2021-04-06 08:58:57 -0400

    working on banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import HeroBanner from './components/heroBanner';
+
+const App = () => {
   return (
-    <div className='App'>
-      <h1>hello</h1>
-    </div>
+    <>
+      <HeroBanner />
+    </>
   );
-}
+};
 
 export default App;
